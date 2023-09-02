d72472059f954bf6b11b1c76b3131b5fc9500459
Junko011 <junko0flowxd@gmail.com>
2023-09-01 23:08:28 -0600

    Cuadriculas

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,14 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+
     public int width;
     public int height;
-    public GameObject titleObject;
-
-
+    public GameObject tileObject;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +16,14 @@ public class Board : MonoBehaviour
         SetupBoard();
     }
 
-
-    private void SetupBoard(){
-        for(int x=0; x<width; x++){
-            for(int y=0; y<height; y++)
-            {var o = Instantiate (titleObject, new Vector3(x, y, -5),  Quaternion.identify);
-             o.transform.parent = transform;
+    private void SetupBoard()
+    {
+        for(int x=0; x<width; x++)
+        {
+            for (int y = 0; y<height; y++)
+            {
+                var o = Instantiate(tileObject, new Vector3(x, y, -5), Quaternion.identity);
+                o.transform.parent = transform;
             }
         }
 
@@ -31,6 +33,6 @@ public class Board : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
