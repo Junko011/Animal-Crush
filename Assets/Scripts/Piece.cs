dbf75bce2cfde4fce226b672a8bfe59fca88a297
Junko011 <junko0flowxd@gmail.com>
2023-09-02 21:34:55 -0600

    Piezas
    Agregue piezas

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,6 +4,34 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+    public int x;
+    public int y;   
+    public Board board;
+
+    public enum type
+    {
+        elefante,
+        jirafa,
+        hipo,
+        mono,
+        panda,
+        loro,
+        pinguino,
+        cerdo,
+        conejo,
+        serpiente
+    };
+
+    public type pieceType;
+
+
+    public void Setup(int x_, int y_, Board board_){
+        x=x_;
+        y=y_;       
+        board=board_;
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
